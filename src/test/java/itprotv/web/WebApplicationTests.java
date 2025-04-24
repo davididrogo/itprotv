c9f9cac1e12bfd3556920d0d9d0ef57313ff9fa3
eric idrogo <eric.idrogo_consultor@mdp.com.pe>
2025-04-24 18:05:22 -0500

    Pushing Spring Boot microservice base

---
+++ src/test/java/itprotv/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package itprotv.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
